a86924c00ed9b00cadecfa34e36abe89ff613546
Carlos Bárcena Bescansa <carlos.barcena.bescansa@gmail.com>
2023-07-31 11:20:37 +0200

    Trying to fix Google Sign in

--- client/App.js
+++ client/App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import { ResponseType } from 'expo-auth-session';
+import jwtDecode from 'jwt-decode';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
+    {
+      clientId: '997836173651-suke0lm40ksnhkq4qv50e3u883dv1sgq.apps.googleusercontent.com',
+      redirectUri: 'https://auth.expo.io/@carlosbarbes/landmarklens',
+    },
+    {
+      responseType: ResponseType.Token,
+    }
+  );
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { id_token } = response.params;
+
+      const decodedToken = jwtDecode(id_token);
+      console.log(decodedToken);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        style={styles.googleButton}
+        onPress={() => {
+          promptAsync();
+        }}
+        disabled={!request}
+      >
+        <Text style={styles.buttonText}>Sign in with Google</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +50,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  googleButton: {
+    backgroundColor: '#4285F4',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
 });
+
---
+++ client/GoogleAuth.js
@@ -0,0 +1,60 @@
+
+import React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Button } from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
+
+WebBrowser.maybeCompleteAuthSession();
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+  tokenEndpoint: 'https://www.googleapis.com/oauth2/v4/token',
+  revocationEndpoint: 'https://oauth2.googleapis.com/revoke',
+};
+
+export default function GoogleAuth() {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: '997836173651-suke0lm40ksnhkq4qv50e3u883dv1sgq.apps.googleusercontent.com',
+      scopes: ['openid', 'profile', 'email'],
+      redirectUri: makeRedirectUri({
+        native: 'com.carlosbarbes.landmarklens:/oauth2redirect',
+        useProxy: false,
+      }),
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+
+      const fetchAccessToken = async () => {
+        try {
+          const tokenResponse = await Google.exchangeCodeAsync({
+            clientId: '997836173651-suke0lm40ksnhkq4qv50e3u883dv1sgq.apps.googleusercontent.com',
+            code,
+          });
+          console.log(tokenResponse);
+        } catch (err) {
+          console.error("Error fetching access token", err);
+        }
+      };
+
+      fetchAccessToken();
+    }
+  }, [response]);
+
+  return (
+    <Button
+      disabled={!request}
+      title="Login with Google"
+      onPress={() => {
+        promptAsync();
+      }}
+    />
+  );
+}
+
